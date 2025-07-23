ea2394b648c6cd519c59f101bcac2c9928304b1c
Bruno Esteves <n3586@hotmail.com>
2025-07-23 16:48:49 -0300

    remove old

--- old/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "public/index.php";
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("Core/" . $class . '.php');
-});
-
